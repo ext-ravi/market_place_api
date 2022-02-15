f60d4e32a544c34c81780927bacb8d303b03b4ea
ravigaikwad786 <ravigaikwad786@gmail.com>
2022-02-15 14:52:21 +0530

    Restrict action for unauthorise user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < ApplicationController
     
-    before_action :set_user, onely: %i[show update destroy] 
+    before_action :set_user, only: %i[show update destroy] 
+    before_action :check_owner, only: %i[update destroy]
     
     def show
         render json: @user
@@ -25,6 +26,7 @@ class Api::V1::UsersController < ApplicationController
     end
     
     def destroy
+      byebug
         @user.destroy
         head 204
     end
@@ -38,5 +40,9 @@ class Api::V1::UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:email, :password)
+    end
+
+    def check_owner
+      head :forbidden unless @user.id == current_user&.id
     end
 end
--- test/controllers/api/v1/tokens_controller_test.rb
+++ test/controllers/api/v1/tokens_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class Api::V1::TokensControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get api_v1_tokens_create_url
-    assert_response :success
+  setup do
+    @user = user(:one)
   end
+
+  test 'should get JWT token' do 
+    post api_v1_token_url, params: { user: {email:@user.email , password:'hdidndi@'}}, as: :json
+    assert_response :success
+
+    json_response = JSON.parse(responce.body)
+    assert_not_nil json_response['token']
+  end
+
+  test 'shoud not get JWT token' do
+    post api_v1_token_url, params: { user: {email:@user.email, password:'rww'}}, as: :json
+    assert_response :unauthorized
+  end
+
 end
