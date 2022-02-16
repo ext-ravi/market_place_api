dadd19d9a78b0f84265aa5f7242feb46d3f0af33
ravigaikwad786 <ravigaikwad786@gmail.com>
2022-02-16 18:52:44 +0530

    Add product serializer

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,26 +4,33 @@ class Api::V1::ProductsController < ApplicationController
   before_action :check_owner, only: %i[update destroy]
 
   def show
-    render json: Product.find(params[:id])
+    render json: ProductSerializer.new(@product).serializable_hash
   end
 
   def index
-    render json: Product.all
+    render json: ProductSerializer.new(Product.all).serializable_hash
   end
 
   def create
-    byebug
     product = current_user.products.build(product_params)
     if product.save
-      render json: product, status: :created
+      render json: ProductSerializer.new(product).serializable_hash, status: :created
     else
       render json: { errors: product.errors }, status: :unprocessable_entity
     end
   end
 
   def destroy
-      @product.destroy
-      head 204
+    @product.destroy
+    head 204
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: ProductSerializer.new(@product).serializable_hash  
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
   end
 
   private
@@ -37,6 +44,6 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def set_product
-      @product = Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 end
