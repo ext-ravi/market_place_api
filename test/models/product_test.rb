4487df8d05b11d85468c5bfa837366aaac1e6bca
ravigaikwad786 <ravigaikwad786@gmail.com>
2022-02-17 13:46:19 +0530

    adds search scope on product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,25 +2,27 @@ require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
   test "should filter products by name" do
-    assert_equal 2, Product.filter_by_title('tv').count
+    assert_equal 2, Product.filter_by_title("tv").count
   end
 
-
   test "should filter products by name and sort them " do
-    assert_equal [products(:another_tv),products(:one)],
-    Product.filter_by_title('tv').sort
+    assert_equal [products(:another_tv), products(:one)],
+                 Product.filter_by_title("tv").sort
   end
 
   test "should filter product by price and sort them" do
-    assert_equal [products(:two),products(:one)],
-    Product.above_or_equal_to_price(200).sort
+    assert_equal [products(:two), products(:one)],
+                 Product.above_or_equal_to_price(200).sort
   end
 
   test "should sort product by most recent" do
     products(:two).touch
-    assert_equal [products(:another_tv),products(:one),products(:two)],
-    Product.recent.to_a
-
+    assert_equal [products(:another_tv), products(:one), products(:two)],
+                 Product.recent.to_a
   end
 
+  test "search should not find videogame and 100 as min price" do
+    search_hash = { keyword: "videogame", min_price: 100 }
+    assert_equal Product.search(search_hash).empty?
+  end
 end
