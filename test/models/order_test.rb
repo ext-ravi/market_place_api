a386244b498ecc13282cf94508134a97d67454f2
ravigaikwad786 <ravigaikwad786@gmail.com>
2022-02-18 13:58:50 +0530

    Adds the create method for the orders controller

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  test "should have positive total" do
+  test 'Should have a positive total' do
     order = orders(:one)
     order.total = -1
     assert_not order.valid?
-  end
+    end
 end
